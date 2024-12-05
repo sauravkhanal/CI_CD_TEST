ee1376086197dcebfc07bee2cc0ca09050dacf63
sauravkhanal <sauravkhanal635@gmail.com>
2024-12-05 22:24:53 +0545

    Finally: CICD complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [showFullDetails, setShowFullDetails] = useState(false)
+  console.log(showFullDetails)
   return (
     <>
       <div>
@@ -25,6 +26,15 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <p>The development mode is: {import.meta.env?.VITE_DEPLOYMENT || "Unspecified"}</p>
+        <p title='click for full details'
+          style={{ cursor: 'pointer' }}
+          onClick={() => setShowFullDetails((prev) => !prev)}
+        >Finally setup in server side is complete.</p>
+        <p style={{ display: showFullDetails ? 'block' : "none", maxWidth: "512px" }}>
+          The original intention was to specify the port using GitHub Secrets, so the Docker container could run on the given port in the server (which is done). However, a problem arose when I attempted to dynamically set the Nginx configuration to map <code>[port_number].mydomainname.com</code> to <code>localhost:[port_number]</code>. The issue was due to skill limitations, so this task has been added to the backlog. 😅
+          <hr className="my-2 border-gray-300" />
+          One of the major goals was to set up a CI/CD pipeline to deploy the site on pushes to the <code>dev-deployment</code> branch, which has been successfully completed. Hurray! 🎉
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
